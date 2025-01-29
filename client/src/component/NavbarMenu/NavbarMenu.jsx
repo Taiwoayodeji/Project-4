81a684950912a8910608d9917a4643f67496af4b
Taiwo Ayodeji <personal.taiwoayodeji@gmai.com>
2025-01-29 17:16:04 -0600

    Database Connected

--- client/src/component/NavbarMenu/NavbarMenu.jsx
+++ client/src/component/NavbarMenu/NavbarMenu.jsx
@@ -5,7 +5,7 @@ export default function NavbarMenu({ user }) {
   return (
     <Navbar expand="lg" className="bg-white text-dark" variant="light" sticky="top">
       <Container>
-        <Navbar.Brand href="/">Q/A</Navbar.Brand>
+        <Navbar.Brand href="/login">Q/A</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="d-flex justify-content-between">
           <Nav>
