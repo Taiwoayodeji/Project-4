14e6a0edb66597be94ffa84e8c3077e1eeca735e
Taiwo Ayodeji <personal.taiwoayodeji@gmai.com>
2025-02-07 13:35:38 -0600

    All Features Added & Functional

--- client/src/component/NavbarMenu/NavbarMenu.jsx
+++ client/src/component/NavbarMenu/NavbarMenu.jsx
@@ -8,29 +8,31 @@ export default function NavbarMenu({ user, handleSignOut }) {
       <Container>
         <Navbar.Brand as={Link} to="/questions">Q/A</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="d-flex justify-content-between">
+        <Navbar.Collapse id="basic-navbar-nav">
+  
+          {/* Right-aligned navigation */}
           <Nav>
-            {/* Conditionally render "Sign Up" or "Questions" link */}
             {user?.user_id ? (
               <Nav.Link as={Link} to="/questions">Questions</Nav.Link>
             ) : (
               <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
             )}
           </Nav>
-          <Nav className="d-flex align-items-center">
+
+          <Nav className="ms-auto">
             {user?.user_id ? (
               <>
                 <span className="me-3">Good Morning, {user?.user_name}</span>
                 <button
                   onClick={handleSignOut}
-                  className="btn btn-danger me-2"
+                  className="btn btn-danger"
                 >
                   Sign Out
                 </button>
               </>
             ) : (
               <Link to="/login">
-                <button className="btn btn-primary me-2">
+                <button className="btn btn-primary">
                   Login
                 </button>
               </Link>
